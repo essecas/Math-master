081178ecd41c53ae8bf392a4ffdeab1f3b31fddd
essecas <80982283+essecas@users.noreply.github.com>
2026-02-12 13:56:13 +0700

    Update vite.config.js

--- Kurikulum Cerdas Matematika 3/vite.config.js
+++ Kurikulum Cerdas Matematika 3/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+export default defineConfig({
+  plugins: [react()],
+  base: '/',  // <-- tambah ini kalau belum ada
+})
 export const vitePort = 3000;
 
 export default defineConfig(({ mode }) => {
